613b4b6b8c6e69fb46718829a386e7585736ba8d
atian155 <atianthony@outlook.com>
2024-03-16 12:58:33 +0200

    Added JWT functions

--- backend/config.php
+++ backend/config.php
@@ -1,1 +1,34 @@
 <?php
+$db_host = "localhost";
+$db_user = "root";
+$db_pass = "";
+$db_name = "flightdb";
+
+$mysqli = new mysqli($db_host, $db_user, $db_pass, $db_name);
+if ($mysqli->connect_error) {
+  exit("Connection to MySQL failed. Error: " . $mysqli->connect_error);
+}
+
+// JWT: https://dev.to/robdwaller/how-to-create-a-json-web-token-using-php-3gml
+function makeJWT(array $payload = []): string
+{
+  $header = json_encode(['typ' => 'JWT', 'alg' => 'HS256']);
+  $payload = json_encode($payload);
+  $base64UrlHeader = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($header));
+  $base64UrlPayload = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($payload));
+  $signature = hash_hmac('sha256', $base64UrlHeader . "." . $base64UrlPayload, 'abcDEF123098', true);
+  $base64UrlSignature = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($signature));
+
+  return $base64UrlHeader . "." . $base64UrlPayload . "." . $base64UrlSignature;
+}
+
+function verifyJWT(string $token)
+{
+  $parts = explode(".", $token);
+  if (count($parts) < 3) {
+    return false;
+  }
+  $payload = json_decode(base64_decode($parts[1]), true);
+  $ver = makeJWT($payload);
+  return $ver === $token;
+}
